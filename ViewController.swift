528692cd833667526a0bd529a1aed3baaacf0e96
Daniel Zastashkov <zast2020@mail.ru>
2023-08-28 16:28:59 +0300

    remove storyboard, start code layout

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Avito
+//
+//  Created by Даниил on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
